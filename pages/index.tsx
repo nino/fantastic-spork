f3ee630cbd866cb3556b20693683c87d3e7b9f96
Nino Annighoefer <nino@ninoan.com>
2022-08-13 19:41:53 +0100

    Make a change to see how fast it redeploys

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ const Home: NextPage = () => {
     <main className="container px-4 mx-auto">
       <h1 className="text-center font-bold text-2xl m-4">Hello</h1>
       <p className="py-2">This is some text.</p>
+      <p className="py-2">Here is some more text.</p>
     </main>
   );
 };
