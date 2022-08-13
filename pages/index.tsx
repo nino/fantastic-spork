d2cc0c0eba3270e5e00a39fbd810c07422984d01
Nino Annighoefer <nino@ninoan.com>
2022-08-13 20:18:16 +0100

    Try setting up HappyKit feature flags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from "next";
 import classNames from "classnames";
+import { useFlags } from "@happykit/flags/client";
 
 const Home: NextPage = () => {
+  const { flags } = useFlags();
+
   return (
     <main className="container px-4 mx-auto">
       <h1 className="text-center font-bold text-2xl m-4">Hello</h1>
       <p className="py-2">This is some text.</p>
       <p
         className={classNames("py-2", {
-          "text-red-600": process.env.NEXT_PUBLIC_RED,
+          "text-red-600": flags?.red_text,
         })}
       >
         Here is some more text.
