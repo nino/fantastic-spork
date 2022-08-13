9d296007b0d509a66c44f746e7455cc603cf300e
Nino Annighoefer <nino@ninoan.com>
2022-08-13 19:50:20 +0100

    Is this a feature flag?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import type { NextPage } from "next";
+import classNames from "classnames";
 
 const Home: NextPage = () => {
   return (
     <main className="container px-4 mx-auto">
       <h1 className="text-center font-bold text-2xl m-4">Hello</h1>
       <p className="py-2">This is some text.</p>
-      <p className="py-2">Here is some more text.</p>
+      <p
+        className={classNames("py-2", {
+          "text-red-600": process.env.NEXT_PUBLIC_RED,
+        })}
+      >
+        Here is some more text.
+      </p>
     </main>
   );
 };
